42ef56c7a307adff27dca8e494b286bff25122d7
Shruti <snktiwari1@gmail.com>
2025-05-05 00:01:27 +0530

    submodule removed

---
+++ server/controllers/sellerControllers.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+// Login Seller : /api/seller/login
+
+export const sellerLogin = async (req,res) =>{
+   try{
+    const {email, password}  = req.body;
+    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL)
+    {
+        const token = jwt.sign(({email},process.env.JWT_SECRET,{expiresIn:'7d'}));
+        res.cookie('sellerToken',token,{
+            httpOnly:true,//Prevent JS to access cookie
+            secure:process.env.NODE_ENV === 'production', //use secure cookies in production
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+            maxAge:7 * 24 * 60 * 60 * 1000, // cookie expiration time
+ });
+ return res.json({success:true,message:'Logged In'});
+    }else{
+        return res.json({success:false,message:"Invalid credentials"});
+    }
+   }catch(error)
+   {
+    console.log(error.message);
+    res.json({success:false,message:error.message});
+   }
+}
+
+//Check Seller isAuth : /api/seller/is-auth
+export const isSellerAuth = async(req,res)=>{
+    try{
+        return res.json({success:true})
+
+    }catch(error)
+    {
+        res.json({success:false,message:error.message});
+    }
+}
